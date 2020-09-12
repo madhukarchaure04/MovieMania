42460b958e30b2a1c7269ad2a7cf4bc83e4eab35
Madhukar Chaure (Mindtree LTD) <v-chmadh@microsoft.com>
2020-09-12 22:48:30 +0530

    Added API authentication

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,47 @@
 import React, { Component } from "react";
+import "./Home.css";
 
 class Home extends Component {
+  state = {
+    message: [],
+  };
+
+  componentDidMount() {
+    fetch("/movies", {
+      headers: { Authorization: `Bearer ${this.props.auth.getAccessToken()}` },
+    })
+      .then((response) => {
+        if (response.ok) return response.json();
+        throw new Error("Network response was not ok.");
+      })
+      .then((response) => {
+        console.log(response);
+        this.setState({ message: response.movies });
+      })
+      .catch((error) => {
+        console.log("User is not logged in");
+      });
+  }
   render() {
     return (
-      <div>
-        <h2>Home</h2>
-      </div>
+      <>
+        {this.props.auth.isAuthenticated() ? (
+          <div>
+            <h2 className="center">Movie Catalog</h2>
+            {this.state.message.map((item) => (
+              <div>{item.Title}</div>
+            ))}
+          </div>
+        ) : (
+          <div className="error">
+            Please {/* eslint-disable-next-line */}
+            <a href="#" onClick={this.props.auth.login}>
+              login
+            </a>{" "}
+            to see the movie catalog
+          </div>
+        )}
+      </>
     );
   }
 }
