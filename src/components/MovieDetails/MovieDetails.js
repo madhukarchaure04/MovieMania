a97574a15d33cd31b4e7954b50d08162f06d6a18
Madhukar Chaure (Mindtree LTD) <v-chmadh@microsoft.com>
2020-09-12 23:55:19 +0530

    Added movies details page

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -7,7 +7,41 @@ export default class MovieDetails extends React.Component {
     return (
       <div>
         <h2 className="center">Movie Details</h2>
-        {this.props.movie.Title}
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12 movietitle">{this.props.movie.Title}</div>
+            <div className="col-md-2"></div>
+            <div className="col-sm-12 col-md-6 rowformat">
+              IMDb Rating: {this.props.movie.imdbRating}
+            </div>
+            <div className="col-sm-12 col-md-4 rowformat">
+              {this.props.movie.listingType === "NOW_SHOWING"
+                ? "NOW SHOWING"
+                : ""}
+            </div>
+            <div className="col-sm-12 movieposter spacer">
+              <img src={this.props.movie.Poster} alt="Movie Poster"></img>
+            </div>
+            <div className="col-sm-12 description spacer">
+              <q>{this.props.movie.Plot}</q>
+            </div>
+            <div className="col-sm-12 rowformat ">
+              <b>Language:</b> {this.props.movie.Language}
+            </div>
+            <div className="col-sm-12 rowformat ">
+              <b>Location:</b> {this.props.movie.Location}
+            </div>
+            <div className="col-sm-12 rowformat spacer">
+              <b>Sound Effects:</b>{" "}
+              {this.props.movie.SoundEffects.map((effect) => (
+                <span>"{effect}" </span>
+              ))}
+            </div>
+            <div className="col-sm-12 description spacer">
+              Option to book the movie is comming soon
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
