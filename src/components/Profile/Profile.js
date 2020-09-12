e969ce9a3a593c9a1141cd9f06d13968fb934264
Madhukar Chaure (Mindtree LTD) <v-chmadh@microsoft.com>
2020-09-13 00:14:21 +0530

    Modified profile page

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import { Card } from "react-bootstrap";
+import "./Profile.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 class Profile extends Component {
   state = {
@@ -20,16 +23,22 @@ class Profile extends Component {
     const { profile } = this.state;
     if (!profile) return null;
     return (
-      <>
-        <h2>Profile</h2>
-        <img
-          style={{ maxWidth: 50, maxHeight: 50 }}
-          src={profile.picture}
-          alt="profile pic"
-        />
-        <p>Name: {profile.nickname}</p>
-        <p>Email: {profile.email}</p>
-      </>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4 col-sm-12 profile">
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src={profile.picture} />
+              <Card.Body>
+                <Card.Title>{profile.name}</Card.Title>
+                <Card.Text>{profile.email}</Card.Text>
+              </Card.Body>
+            </Card>
+          </div>
+          <div className="col-md-8 col-sm-12 description bookinginfo">
+            Movies booking info will be shown here in future
+          </div>
+        </div>
+      </div>
     );
   }
 }
